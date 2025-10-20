44f492e7b4c1c7e27427842069ac1c32429f2d18
Muchwat <kevinmuchwat@gmail.com>
2025-10-20 03:32:44 +0300

    update

--- wp-content/themes/cipitcustom/page-artificial-intelligence.php
+++ wp-content/themes/cipitcustom/page-artificial-intelligence.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
+<div class="breadcrumbs">
+    <div>
+        <h3>Artificial Intelligence</h3>
+        <a href="<?php echo esc_url(home_url('/')); ?>">Home</a> / <span>Research</span> / Artificial Intelligence
+    </div>
+</div>
+
+<!-- Blog Header -->
+<section class="blog-header">
+    <h1>Artificial Intelligence</h1>
+    <p>Insights, analysis, and commentary on artificial intelligence, information technology law, and digital
+        policy in Africa</p>
+</section>
 <?php the_content(); ?>
 <?php get_footer(); ?>
